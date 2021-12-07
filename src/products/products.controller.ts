cf8e43997c8a7b826beafaa94bed575c4244e9e9
Leonardo Schneider <lnrdschneider@gmail.com>
2021-12-07 11:35:50 -0300

    updating value with PATCH

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common';
+import { stringify } from 'querystring';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -26,5 +27,16 @@ export class ProductsController {
   @Get(':id')
   getProduct(@Param('id') prodId: string) {
     return this.productsService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
   }
 }
