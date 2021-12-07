47d67934a1a6c5ee561ca4c17e3ebce54d8780fd
Leonardo Schneider <lnrdschneider@gmail.com>
2021-12-07 11:04:31 -0300

    a single product GET by produId

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -17,8 +17,14 @@ export class ProductsController {
     );
     return { id: generatedId };
   }
+
   @Get()
   getAllProducts() {
     return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Product } from './product.model';
 
@@ -7,12 +7,20 @@ export class ProductsService {
   private products: Product[] = [];
 
   insertProduct(title: string, description: string, price: number) {
-    const prodId = new Date().toString();
+    const prodId = Math.random().toString();
     const newProduct = new Product(prodId, title, description, price);
     this.products.push(newProduct);
     return prodId;
   }
   getProducts() {
     return [...this.products];
+  }
+
+  getSingleProduct(productId: string) {
+    const product = this.products.find((prod) => prod.id === productId);
+    if (!product) {
+      throw new NotFoundException('Could not find the product.');
+    }
+    return { ...product };
   }
 }
